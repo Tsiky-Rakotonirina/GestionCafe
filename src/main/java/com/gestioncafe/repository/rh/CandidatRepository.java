cd7ce1fdc1aadb788292fceeeb5d772ea62f1981
MichaelETU003327 <mikermb18@gmail.com>
2025-06-24 10:24:08 +0300

    Creation de repository correspondant à chaque modele utile au parametrage

---
+++ src/main/java/com/gestioncafe/repository/rh/CandidatRepository.java
@@ -0,0 +1,9 @@
+package com.gestioncafe.repository.rh;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestioncafe.model.rh.Candidat;
+
+public interface CandidatRepository extends JpaRepository<Candidat, Long> {
+
+}
