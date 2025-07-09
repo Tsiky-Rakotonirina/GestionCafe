5c6f39082ec6b709f89567f30048be217f4abe98
TonyRandrian <tony.randrian855@gmail.com>
2025-07-09 10:28:32 +0300

    finish merg start debug and test

--- src/main/java/com/gestioncafe/repository/StatutEmployeRepository.java
+++ src/main/java/com/gestioncafe/repository/StatutEmployeRepository.java
@@ -1,13 +1,32 @@
 package com.gestioncafe.repository;
 
-import com.gestioncafe.model.StatutEmploye;
-import com.gestioncafe.model.Employe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.gestioncafe.model.*;
+
+@Repository
 public interface StatutEmployeRepository extends JpaRepository<StatutEmploye, Long> {
+    @Query(value = """
+        SELECT se.* FROM statut_employe se
+        INNER JOIN (
+            SELECT id_employe, MAX(date_statut) AS max_date
+            FROM statut_employe
+            GROUP BY id_employe
+        ) last_status ON se.id_employe = last_status.id_employe AND se.date_statut = last_status.max_date
+        WHERE se.id_statut = :idStatut
+    """, nativeQuery = true)
+    public List<StatutEmploye> findDerniersStatutsParEmployeEtStatut(@Param("idStatut") Long idStatut);
+    public Optional<StatutEmploye> findTopByEmploye_IdOrderByDateStatutDesc(Long idEmploye);
+    public Optional<StatutEmploye> findTopByEmploye_IdAndDateStatutLessThanEqualOrderByDateStatutDesc(Long employe_id, LocalDateTime dateStatut);
+
     List<StatutEmploye> findByEmployeOrderByDateStatutDesc(Employe employe);
-    List<StatutEmploye> findByStatut_Id(Long statutId);
+    List<StatutEmploye> findByIdStatut(Long statutId);
 
 }
